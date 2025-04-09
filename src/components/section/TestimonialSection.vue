ce54d0d166fc47fad22b95ba90b9b740ed68c62b
dev-nirob1 <dev.nirob1@gmail.com>
2025-04-09 23:02:50 +0600

    testimonials card design

--- src/components/section/TestimonialSection.vue
+++ src/components/section/TestimonialSection.vue
@@ -1,28 +1,24 @@
 <script setup lang="ts">
 import BaseTitle from '../element/BaseTitle.vue';
 import SubTitle from '../element/SubTitle.vue';
-
+import TestimonialsCard from '../widgets/TestimonialsCard.vue';
 </script>
 
 <template>
   <div class="container py-5">
-    <div class="medium-7">
+    <div class="medium-8">
       <div class="span-3">
         <SubTitle>Our Clients</SubTitle>
         <BaseTitle>Testimonials</BaseTitle>
       </div>
-      <div class="span-4">
-        <!-- Horizontal scroll container -->
+      <div class="span-5">
         <div class="cards-scroll-container">
-          <div class="cards-wrapper">
-            <div class="button testimonial-card">
-              
-            </div>
-            <div class="button card">card 2</div>
-            <div class="button card">card 3</div>
-            <div class="button card">card 4</div>
-            <div class="button card">card 5</div>
-          </div>
+            <TestimonialsCard />
+            <TestimonialsCard />
+            <TestimonialsCard />
+            <TestimonialsCard />
+            <TestimonialsCard />
+            <TestimonialsCard />
         </div>
       </div>
     </div>
@@ -34,28 +30,12 @@ import SubTitle from '../element/SubTitle.vue';
   width: 100%;
   overflow-x: scroll;
   padding-bottom: 1rem;
-  /* -webkit-overflow-scrolling: touch; */
-}
-
-.cards-wrapper {
   display: flex;
-  gap: 1rem;
-  width: max-content;
-}
-
-.card {
-  flex: 0 0 auto;
-  width: 300px;
-  height: 250px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #ffff;
-  border-radius: 8px;
+  gap: 1.5rem;
 }
 
 .cards-scroll-container::-webkit-scrollbar {
-  height: 5px;
+  height: 10px;
 }
 
 .cards-scroll-container::-webkit-scrollbar-thumb {
---
+++ src/components/widgets/TestimonialsCard.vue
@@ -0,0 +1,56 @@
+<script setup>
+import BaseImage from '../element/BaseImage.vue';
+import BaseParagraph from '../element/BaseParagraph.vue';
+
+
+</script>
+
+<template>
+  <div class="testimonial">
+    <div class="flex align-center gap-1">
+      <BaseImage image="/author.jpg" />
+      <div>
+        <h5 class="text-dark">Adam Josheph</h5>
+        <BaseParagraph>
+          Selling Agent
+        </BaseParagraph>
+      </div>
+    </div>
+    <div class="flex">
+      <BaseParagraph>
+        <i class="fa-solid fa-quote-left fa-2x"></i> Precious ipsum dolor sit amet
+        consectetur adipisicing elit, sed dos
+        mod tempor incididunt ut labore et
+        dolore magna aliqua. Ut enim ad min
+        veniam, quis nostrud Precious ips
+        um dolor sit amet, consecte
+      </BaseParagraph>
+    </div>
+
+  </div>
+</template>
+<style scoped>
+.testimonial {
+  flex: 0 0 auto;
+  padding: 1.5rem;
+  width: 300px;
+  height:280px;
+  background: #ffff;
+  border-radius: 8px;
+}
+
+.testimonial h5 {
+  margin: 0;
+  padding: 0;
+}
+
+.testimonial p {
+  margin: 10px 0 0 0;
+}
+
+.testimonial img {
+  height: 4rem;
+  width: 4rem;
+  border-radius: 50%;
+}
+</style>
