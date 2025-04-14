9a38c749e35e20828bf9edfbc05e83ffb135e1b9
dev-nirob1 <dev.nirob1@gmail.com>
2025-04-14 21:30:25 +0600

    registration page

--- src/zems/front/pages/RegisterPage.vue
+++ src/zems/front/pages/RegisterPage.vue
@@ -6,38 +6,54 @@ import InputField from '@/components/element/InputField.vue';
 
 <template>
   <section class="container">
-    <BaseTitle>
+    <BaseTitle class="text-center">
       Create an Account
     </BaseTitle>
-    <div class="medium-2 gap-2">
+    <div class="medium-2 align-center gap-2">
       <div>
-
+        <img class="width-full height-full"
+          src="https://plus.unsplash.com/premium_photo-1681487814165-018814e29155?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="">
       </div>
       <div>
         <form>
+          <div class="all-2 gap-1">
+            <InputField placeholder="First Name" />
+            <InputField placeholder="Last Name" />
+          </div>
+          <div>
+            <InputField placeholder="Enter Your Email" />
+          </div>
+          <div>
+            <InputField placeholder="Enter Your Password" />
+          </div>
+          <div class="flex align-center">
+            <InputField type="checkbox" />
+            I aggree to the <RouterLink to="/">terms & conditions</RouterLink>
+          </div>
+          <button class="btn bg-secondary text-white width-full">Create Account</button>
+        </form>
+        <div>
+          <hr>
+        </div>
         <div class="all-2 gap-1">
-          <InputField placeholder="First Name" />
-          <InputField placeholder="Last Name" />
+          <button class="btn bg-primary text-white">Google</button><button
+            class="btn bg-primary text-white">Apple</button>
         </div>
         <div>
-          <InputField placeholder="Enter Your Email" />
+          <p>Already have an account? <RouterLink to="/login">Login Here</RouterLink></p>
         </div>
-        <div>
-          <InputField placeholder="Enter Your Password" />
-        </div>
-        <div class="flex">
-          <input type="checkbox" name="" id="">
-          aggree
-        </div>
-        <button class="btn bg-secondary text-white width-full">Create Account</button>
-      </form>
-      <div>
-        <hr>
-      </div>
-      <div class="all-2 gap-1">
-        <button class="btn bg-primary text-white">Google</button><button class="btn bg-primary text-white">Apple</button>
-      </div>
       </div>
     </div>
   </section>
 </template>
+<style scoped>
+form input[type=checkbox] {
+  width: fit-content;
+  margin-right: .5rem;
+}
+
+form a {
+  margin-left: .5rem;
+}
+</style>
