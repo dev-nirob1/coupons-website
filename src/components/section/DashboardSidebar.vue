f798bfe0f83f4c18980539d599e43543ae792eb4
dev-nirob1 <dev.nirob1@gmail.com>
2025-04-30 19:58:12 +0600

    update dashbard navbar with mobile responsive

--- src/components/section/DashboardSidebar.vue
+++ src/components/section/DashboardSidebar.vue
@@ -7,28 +7,48 @@ const toggleMenu = () => {
 }
 </script>
 <template>
-  <aside :class="{ 'is-expanded': isExpanded }">
-    <div class="logo">
-      <img src="/logo.png" alt="logo">
+  <div class="small" :class="{ 'collapsed': isExpanded }">
+    <div class="small-nav">
+      <!-- toggle menu bar  -->
+      <div class="menu-toggle">
+        <BaseButton @click="toggleMenu">
+          <i :class="isExpanded ? 'fas fa-xmark' : 'fas fa-bars'"></i>
+        </BaseButton>
+      </div>
+      <div class="logo">
+        <img src="/logo.png" alt="logo">
+      </div>
+
     </div>
-    <!-- toggle menu bar  -->
-    <div class="menu-toggle">
-      <BaseButton @click="toggleMenu">
-        <i :class="isExpanded ? 'fas fa-xmark' : 'fas fa-bars'"></i>
-      </BaseButton>
+    <div class="menu">
+      <RouterLink to="/dashboard">
+        <i class="fa-solid fa-house-user"></i>
+        <span>Home</span>
+      </RouterLink>
+      <RouterLink to="/add-coupon">
+        <i class="fa-solid fa-plus"></i>
+        <span>Add Coupon</span>
+      </RouterLink>
+      <RouterLink to="/coupons-management">
+        <i class="fa-solid fa-list-check"></i>
+        <span>Manage Coupon</span>
+      </RouterLink>
+      <RouterLink to="/categories-management">
+        <i class="fa-solid fa-layer-group"></i>
+        <span>Categories</span>
+      </RouterLink>
+      <RouterLink to="/users-management">
+        <i class="fa-solid fa-users"></i>
+        <span>Users</span>
+      </RouterLink>
     </div>
-  </aside>
+  </div>
   <!-- <div class="sidebar">
     <div class="toggle-btn" @click="toggleSidebar">
       <i class="fa-solid fa-bars"></i>
     </div>
     <ul class="nav-links" :class="{ 'active': collapsed }">
-      <li>
-        <RouterLink to="/dashboard">
-          <i class="fa-solid fa-house-user"></i>
-          <span>Home</span>
-        </RouterLink>
-      </li>
+
       <li class="dropdown-parent">
         <div @click="toggleDropdown" class="flex align-center justify-between">
           <div>
@@ -38,27 +58,15 @@ const toggleMenu = () => {
           <i class="fa-solid fa-angle-down"></i>
         </div>
         <div class="dropdown-child" v-if="dropdownCollapse">
-          <RouterLink to="/add-coupon">
-            <i class="fa-solid fa-plus"></i>
-            <span>Add Coupon</span>
-          </RouterLink>
-          <RouterLink to="/coupons-management">
-            <i class="fa-solid fa-list-check"></i>
-            <span>Manage Coupon</span>
-          </RouterLink>
+
+
         </div>
       </li>
       <li>
-        <RouterLink to="/categories-management">
-          <i class="fa-solid fa-layer-group"></i>
-          <span>Categories</span>
-        </RouterLink>
+
       </li>
       <li>
-        <RouterLink to="/users-management">
-          <i class="fa-solid fa-users"></i>
-          <span>Users</span>
-        </RouterLink>
+
       </li>
     </ul>
     <ul class="profile">
@@ -77,9 +85,106 @@ const toggleMenu = () => {
 </template>
 
 <style scoped>
-aside {
+.small {
+  background-color: var(--primary-color);
+  color: var(--white-color);
+  max-width: 300px;
+}
+
+.small i {
+  color: var(--white-color);
+}
+
+.logo {
+  width: 60px;
+  height: 60px;
+}
+
+.logo img {
+  width: 100%;
+  height: 100%;
+}
+
+.small-nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+.collapsed .menu {
+  top: 4rem;
+  left: 0;
+}
+
+.menu {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  font-size: 1.5rem;
+  position: absolute;
+  left: -120%;
+  background-color: var(--primary-color);
+  height: 100vh;
+  width: fit-content;
+  transition: .3s;
+}
+
+.menu a {
+  display: flex;
+  gap: 1rem;
+  text-decoration: none;
+  padding: .75rem 0;
+}
+
+@media (min-width: 768px) {
+  .small {
+    width: 300px;
+  }
+
+  .menu {
+    background-color: var(--light-color);
+    position: static;
+    left: 0;
+    width: 100%;
+  }
+}
+
+/* @media (min-width: 768px) {
+  .small-nav {
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  .menu {
+    width: 4rem;
+  }
+
+  .is-expanded .menu {
+    position: static;
+    width: 100%;
+    font-size: 1.25rem;
+    overflow: hidden;
+  }
+} */
+
+
+/* .is-expanded {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
   position: fixed;
+  top: 0;
+  width: calc(2rem + 2rem);
+  overflow-y: auto;
+  height: 100vh;
+  background-color: var(--primary-color);
+  color: var(--white-color);
   z-index: 99;
+  transition: .4s;
+}
+
+aside i {
+  font-size: 2rem;
+  color: var(--white-color);
 }
 
 .logo {
@@ -90,7 +195,12 @@ aside {
 .logo img {
   width: 100%;
 }
-
+.is-expanded{
+  width: 60%;
+}
+.is-expanded .menu-toggle {
+    top: -4.5rem;
+  }
 .menu-toggle {
   display: flex;
   justify-content: flex-end;
@@ -99,35 +209,22 @@ aside {
   top: 0;
   transition: .3s;
 }
-.menu-toggle i {
-  font-size: 2rem;
-  color: var(--white-color);
-}
 
-@media (min-width: 768px) {
+.menu {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+} */
+
+/* @media (min-width: 768px) {
   aside {
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
     position: sticky;
     top: 0;
-    width: calc(2rem + 2rem);
-    overflow-y: auto;
-    height: 100vh;
-    background-color: var(--primary-color);
-    color: var(--white-color);
-    transition: .4s;
   }
-
   .is-expanded {
     width: 300px;
   }
 
 
-
-  .is-expanded .menu-toggle {
-    top: -4.5rem;
-  }
-
-}
+} */
 </style>
--- src/zems/back/layout/DashboardLayout.vue
+++ src/zems/back/layout/DashboardLayout.vue
@@ -1,27 +1,25 @@
 <script setup>
-import DashboardNavbar from '@/components/section/DashboardNavbar.vue';
 import DashboardSidebar from '@/components/section/DashboardSidebar.vue';
 import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div class="flex">
-    <div class="sidebar">
-      <DashboardSidebar />
-    </div>
-    <div class="width-full router-view">
-      <DashboardNavbar />
+  <div class="medium-12">
+    <DashboardSidebar class="medium-span-2" />
+    <!-- <DashboardNavbar /> -->
+    <div class="medium-span-10">
       <RouterView />
     </div>
   </div>
 </template>
 <style scoped>
-.sidebar {
+/* .sidebar {
   max-width: 300px;
-}
+} */
+/*
 .router-view {
+  padding-left: 4rem;
   height: 100vh;
   overflow-y: auto;
-
-}
+} */
 </style>
