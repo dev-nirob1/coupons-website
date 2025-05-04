be59bba85f237e09013efe1fd7909b0d8b9a1bac
dev-nirob1 <dev.nirob1@gmail.com>
2025-05-04 22:43:44 +0600

    font added

--- src/zems/back/Components/Widgets/PopEdit.vue
+++ src/zems/back/Components/Widgets/PopEdit.vue
@@ -1,4 +1,6 @@
 <script setup>
+import InputField from '@/components/Element/InputField.vue';
+
 const props = defineProps({
   data: {
     type: Object
@@ -7,5 +9,23 @@ const props = defineProps({
 console.log('popEdit data', props.data);
 </script>
 <template>
-  <h1>{{ data }}</h1>
+  <div class="edit-coupon">
+    <form>
+      <InputField :value="data.name"  placeholder="Name" />
+      <InputField :value="data.details"  placeholder="Name" />
+      <InputField :value="data.code"  placeholder="Name" />
+      <InputField :value="data.title"  placeholder="Name" />
+      <InputField :value="data.discount_parcent"  placeholder="Name" />
+      <InputField :value="data.expire_date"  placeholder="Name" />
+      <InputField :value="data.cat_name"  placeholder="Name" />
+      <InputField :v-model="data.exclusive"  placeholder="Name" />
+      {{ data }}
+    </form>
+  </div>
 </template>
+
+<style scoped>
+.edit-coupon {
+  padding: 2rem;
+}
+</style>
--- src/zems/back/Components/Widgets/PopView.vue
+++ src/zems/back/Components/Widgets/PopView.vue
@@ -7,5 +7,28 @@ defineProps({
 </script>
 
 <template>
+  <div class="large-2 p-2">
+    <div>
+      {{ data?.name }}
+    </div>
+    <div>
+      {{ data?.cat_name }}
+    </div>
+    <div>
+      {{ data?.details }}
+    </div>
+    <div>
+      {{ data?.code }}
+    </div>
+    <div>
+      {{ data?.discount_percent }}%
+    </div>
+    <div>
+      {{ data?.expire_date }}
+    </div>
+    <div>
+      {{ data?.title }}
+    </div>
+  </div>
   {{data}}
 </template>
