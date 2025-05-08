b958d9de96535219499b0515efc13ca1d0d882be
dev-nirob1 <dev.nirob1@gmail.com>
2025-05-08 14:31:07 +0600

    category section link button added

--- src/zems/front/Components/Section/CategorySection.vue
+++ src/zems/front/Components/Section/CategorySection.vue
@@ -12,7 +12,7 @@ defineProps({
 </script>
 
 <template>
-  <section id="category" class="categories">
+  <section class="categories">
     <div class="container">
       <BaseTitle class="text-dark flex">
         Browse <span class="text-primary">Categories</span>
@@ -25,6 +25,18 @@ defineProps({
         </template>
         <template v-else>
           <CategoryCard v-for="category in categories" :category="category" :key="category.id" />
+          <!-- link button  -->
+          <RouterLink class="link-card" to="/categories">
+            <div class="flex flex-col justify-center align-center text-center">
+              <div class="image">
+              <BaseImage image="/categories.png" />
+            </div>
+              <div>
+                <SubTitle>Explore All</SubTitle>
+                <BaseParagraph>View our complete collection of products across all categories</BaseParagraph>
+              </div>
+            </div>
+          </RouterLink>
         </template>
       </div>
     </div>
@@ -32,12 +44,36 @@ defineProps({
 </template>
 
 <style scoped>
-#category {
-  transition: all .3s ease;
-}
 .categories {
+  transition: all .3s ease;
   padding: 2rem .5rem;
 }
+
+.link-card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(to bottom, var(--primary-color), var(--secondary-color));
+  box-shadow: var(--box-shadow);
+  color: var(--white-color);
+  padding: 1rem;
+  border-radius: 1rem;
+  transition: all .3s ease-in-out;
+}
+.link-card .sub-title {
+  margin: 0;
+}
+.link-card p {
+  margin-top: .5rem;
+}
+
+.link-card:hover {
+  transform: scale(1.02);
+}
+
 hr {
   flex: 1;
   border: none;
--- src/zems/front/Components/Section/FeaturedCoupon.vue
+++ src/zems/front/Components/Section/FeaturedCoupon.vue
@@ -68,7 +68,7 @@ defineProps({
   text-decoration: none;
   height: 100%;
   width: 100%;
-  background-color: var(--secondary-color);
+  background: linear-gradient(to bottom, var(--primary-color), var(--secondary-color));
   box-shadow: var(--box-shadow);
   color: var(--white-color);
   padding: 1rem;
@@ -79,7 +79,12 @@ defineProps({
 .link-card:hover {
   transform: scale(1.02);
 }
-
+.link-card h5 {
+  margin: 0;
+}
+.link-card p {
+  margin-top: .5rem;
+}
 .link-card img {
   height: 100%;
   width: 100%;
