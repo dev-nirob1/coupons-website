12ddbe3ac75b1c99d415ad3242e909c7e076d0e1
dev-nirob1 <dev.nirob1@gmail.com>
2025-07-26 14:47:17 +0600

    update breadcrumb section

--- src/components/widgets/BreadcrumbSection.vue
+++ src/components/widgets/BreadcrumbSection.vue
@@ -22,30 +22,27 @@ const paramsName = computed(() => route.params.type)
 
 <style scoped>
 .breadcrumb {
-  margin-top: 5.6rem;
-  padding: 3rem .5rem;
-  background:
-   linear-gradient(rgba(255, 255, 255, 0.104), rgba(255, 255, 255, 0.142)),
-    url('https://images.unsplash.com/photo-1620121692029-d088224ddc74?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') no-repeat center / 100%;
+  padding: 8rem 0 3rem 0;
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("https://images.unsplash.com/photo-1733503747506-773e56e4078f?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+      no-repeat center / 100%;
+  /* background: var(--secondary-color); */
 }
-
 .breadcrumb ul {
   list-style: none;
   font-size: 1rem;
   padding: 0;
 }
-
 .breadcrumb ul li a {
   color: var(--primary-color);
   text-decoration: none;
 }
-
 .breadcrumb ul li:hover a {
   text-decoration: underline;
 }
-
 .breadcrumb ul li:not(:last-child)::after {
-  content: '/';
+  content: "/";
+  font-family: monospace;
   padding-left: 1rem;
 }
 
