0cc3f8ee35ca06ba16e8124ebafaeaa4f72a2c7f
dev-nirob1 <dev.nirob1@gmail.com>
2025-07-26 15:05:24 +0600

    update navbar section

--- src/components/section/HeaderSection.vue
+++ src/components/section/HeaderSection.vue
@@ -28,8 +28,15 @@ onMounted(() => {
     <nav class="flex justify-between align-center container">
       <!-- Logo -->
       <router-link to="/" class="logo">
-        <BaseImage src="/logo.png" alt="logo" />
-        <span class="brand">CouponCraze</span>
+         <img
+            class="height-full width-full"
+            src="/logo.png"
+            alt="logo"
+          />
+          <div class="brand">
+            <span>CouponHub</span>
+            <p>Shop Smarter, Save Bigger</p>
+          </div>
       </router-link>
 
       <!-- Mobile Menu Button -->
@@ -44,13 +51,13 @@ onMounted(() => {
         <ListItem><router-link to="/coupon_list/exclusive">Exclusive Coupons</router-link></ListItem>
         <ListItem><router-link to="/coupon_list/ending_soon">Ending Soon</router-link></ListItem>
         <ListItem><router-link to="/categories">Categories</router-link></ListItem>
-        <ListItem><router-link to="/login">Login</router-link></ListItem>
       </ul>
     </nav>
   </header>
 </template>
 
 <style scoped>
+/* Sticky navbar with scroll effects */
 .scroll-down .navbar {
   transform: translate3d(0, -100%, 0);
 }
@@ -62,40 +69,40 @@ onMounted(() => {
 .navbar {
   backdrop-filter: blur(50px);
   color: var(--white-color);
-  padding: 0.75rem 1rem;
+  padding: 0.75rem 0;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   z-index: 999;
-  transition: .3s ease;
+  transition: 0.3s ease;
 }
-
 .navbar a {
   text-decoration: none;
 }
-
 .logo {
   display: flex;
-  align-items: center;
-  font-size: 2.5rem;
+  gap: 0.5rem;
+}
+.logo span {
+  font-size: 1.8rem;
   font-weight: bold;
 }
-
+.logo p {
+  margin-top: -0.5rem;
+  font-size: 0.8rem;
+}
 .logo img {
-  height: 60px;
+  height: 65px;
   width: 100%;
 }
-
 .brand {
   display: none;
 }
-
 .nav-links {
   display: flex;
   align-items: center;
 }
-
 .navbar ul {
   list-style: none;
   position: fixed;
@@ -109,41 +116,44 @@ onMounted(() => {
   align-items: flex-start;
   padding: 2rem;
   gap: 1.5rem;
-  transition: .3s;
+  transition: 0.3s;
 }
-
 .navbar ul.active {
   top: 0;
   left: 0;
 }
-
 .navbar ul li a {
   font-weight: 500;
   transition: color 0.2s ease-in-out;
   position: relative;
   padding: 0.5rem 0;
 }
-
 .navbar ul li a:hover {
   color: var(--alternative-color);
 }
-
-/* Mobile Menu Styles */
+/* dropdown menu  */
+.dropdown-container {
+  position: relative;
+}
+.dropdown-menu {
+  margin-left: 1.5rem;
+  margin-top: 1rem;
+}
+.dropdown-menu a {
+  display: block;
+}
+/* Mobile menu toggle */
 .hamburger {
   display: block;
   color: var(--white-color);
-  height: 2rem;
-  width: 3rem;
   border: none;
   cursor: pointer;
 }
-
 @media (min-width: 992px) {
-  .brand {
+.brand {
     display: block;
     margin: 0;
   }
-
   .navbar ul {
     position: inherit;
     color: var(--white-color);
@@ -152,11 +162,11 @@ onMounted(() => {
     justify-content: flex-end;
     align-items: center;
     background-color: transparent;
-    padding: .75rem 0;
+    padding: 0.75rem 0;
   }
-
+  /* Desktop menu hover effects */
   .navbar ul li a::after {
-    content: '';
+    content: "";
     position: absolute;
     bottom: 0;
     left: 0;
@@ -165,11 +175,28 @@ onMounted(() => {
     background: var(--alternative-color);
     transition: width 0.3s ease;
   }
-
   .navbar ul li a:hover::after {
     width: 100%;
   }
-
+  /* dropdonw menu  */
+  .navbar .dropdown-menu {
+    position: absolute;
+    top: 1.75rem;
+    left: 0;
+    width: 10rem;
+    margin: 0;
+    padding: 1.5rem 1rem;
+    background-color: var(--secondary-color);
+    display: none;
+    transition: all 0.5s;
+  }
+  .navbar .dropdown-container:hover .dropdown-menu {
+    display: block;
+  }
+  .navbar .dropdown-menu a {
+    margin-bottom: 0.25rem;
+  }
+  /* expand navlinks on desktop  */
   .hamburger {
     display: none;
   }
