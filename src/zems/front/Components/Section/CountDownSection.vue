ad285d34102e0067904ba606ec46751d3798e322
dev-nirob1 <dev.nirob1@gmail.com>
2025-07-27 20:49:27 +0600

    statistics section

--- src/zems/front/Components/Section/CountDownSection.vue
+++ src/zems/front/Components/Section/CountDownSection.vue
@@ -4,28 +4,28 @@
       <ul class="small-2 larger-4 gap-2 medium-gap-1 stats-content">
         <!-- individual stats component  -->
         <li>
-          <i class="fa-solid fa-tag"></i>
+          <i class="fa-solid fa-tag fa-3x"></i>
           <div>
             <h3>560+</h3>
             <p>Active Coupons</p>
           </div>
         </li>
         <li>
-          <i class="fa-solid fa-users"></i>
+          <i class="fa-solid fa-users fa-3x"></i>
           <div>
             <h3>197K+</h3>
             <p>Happy Customers</p>
           </div>
         </li>
         <li>
-          <i class="fa-solid fa-store"></i>
+          <i class="fa-solid fa-store fa-3x"></i>
           <div>
             <h3>268+</h3>
             <p>Partner Stores</p>
           </div>
         </li>
         <li>
-          <i class="fa-solid fa-percent"></i>
+          <i class="fa-solid fa-percent fa-3x"></i>
           <div>
             <h3>340+</h3>
             <p>Exclusive Deals</p>
@@ -38,21 +38,20 @@
 
 <style scoped>
 .statistics {
-  padding: 3.75rem 1rem;
-  margin: 0 auto;
+  padding: 3.75rem 0;
 }
-.stats-content {
+.statistics .stats-content {
   list-style: none;
+  padding: 0;
   height: 100%;
   align-items: center;
 }
-.stats-content li {
+.statistics .stats-content li {
   display: flex;
   align-items: center;
   gap: 1rem;
 }
-.stats-content i {
-  font-size: 3rem;
+.statistics .stats-content i {
   height: 100px;
   width: 100px;
   line-height: 100px;
@@ -60,13 +59,14 @@
   border-radius: 1rem;
   background: var(--primary-lighten-color);
 }
-.stats-content p {
+.statistics .stats-content p {
   margin: 0;
 }
 
+
 @media (min-width: 768px) {
   .statistics {
-    padding: 8rem .5rem;
+    padding: 8rem 0;
   }
 }
 </style>
